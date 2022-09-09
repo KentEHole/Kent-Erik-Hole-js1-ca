37f47118c7ae36bb358a7d111539b79a206402c2
Kent Erik Hole <kent.erik@frontendkenterik.no>
2022-09-09 11:03:30 +0200

    Update layout.js
    addet header for every ul

--- script/pages/layout.js
+++ script/pages/layout.js
@@ -14,22 +14,31 @@ function createLayout(APIInfo){
     footer.innerHTML += `
         <ul>
             <li>
+                <h4>Social Media</h4>
+            </li>
+            <li>
                 <a href="${SpaceXInfo.links.twitter}" alt="Link to twitter of SpaceX">${iconTwitter}</a>
             </li>
             <li>
-                <a href="${SpaceXInfo.links.flicker}" alt="Link to Flicker of SpaceX">${iconFlicker}</a> 
+                <a href="${SpaceXInfo.links.flicker}" alt="Link to Flicker of SpaceX">${iconFlickre}</a> 
             </li>
             <li>
-                <a href="${SpaceXInfo.links.website}" alt="Link to website of SpaceX">${iconWebSit}</a>
+                <a href="${SpaceXInfo.links.website}" alt="Link to website of SpaceX">${iconWebsit}</a>
             </li>
         </ul>
         <ul>
-            <li>Ceo></li>
+            <li>
+                <h4>Company Info</h4>
+            </li>
+            <li>Ceo</li>
             <li>${SpaceXInfo.ceo}</li>
             <li>Employees</li>
-            <li>${SpaceXInfo.employess}</li>
+            <li>${SpaceXInfo.employees}</li>
         </ul>
         <ul>
+            <li>
+                <h4>Company Address</h4>
+            </li>
             <li>${SpaceXInfo.headquarters.address}</li>
             <li>${SpaceXInfo.headquarters.city}</li>
             <li>${SpaceXInfo.headquarters.state}</li>
