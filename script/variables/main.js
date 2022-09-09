69928762f0e6946ae10fd6cdbb0e51bda2449ea5
Kent Erik Hole <kent.erik@frontendkenterik.no>
2022-09-09 09:25:14 +0200

    Creat main.js
    This going to be variables for my script.

---
+++ script/variables/main.js
@@ -0,0 +1,54 @@
+// Global Values 
+
+    // Using Strice mode 
+        "use stric";
+
+    // API Values 
+        // MAIN API URL 
+            const URL =  '';
+        // Secundery API URL
+            const URLSecundery = '';
+    // Document Object Model
+        // BODY 
+            const body = document.querySelector('body');
+        // HEADER 
+            const header =  document.querySelector('header');
+        // MAIN
+            const main = document.querySelector('main');
+        // FOOTER 
+            const footer = document.querySelector('footer');
+    // ICON
+        // TWITTER
+            const iconTwitter = '<i class="fa-brands fa-twitter"></i>'; 
+        // FLICER
+            const iconFlickre = '<i class="fa-brands fa-flickr"></i>';
+        // BROWSER
+            const iconFlicker = '<i class="fa-solid fa-browser"></i>';
+        // HOME 
+            const iconHome = '<i class="fa-solid fa-house"></i>';
+        // CONTACT 
+            const iconContact = '<i class="fa-solid fa-align-right"></i>';
+    // COLOR PALLETS
+
+        // Primary Colors 
+            // Light 
+                const colorPL = '#6384BF';
+            // Main
+                const colorP = '#425880';
+            // Dark
+                const colorPD = '#2E3D59';
+
+        // Secondary color
+            // Light 
+                const colorSL = '';
+            // Main 
+                const colorS = '';
+            // Dark 
+                const colorSD = '';
+        
+        // Triangle Color 
+            // Light 
+            // main
+            // Dark 
+        
+        
