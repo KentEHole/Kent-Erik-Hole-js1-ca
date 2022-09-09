7660d6e0cf8cc506a8c42a1e49e20b8cf8230ce2
Kent Erik Hole <kent.erik@frontendkenterik.no>
2022-09-09 12:02:44 +0200

    Create errorHandler.js
    This is the Error handler for this site.

---
+++ script/components/errorHandler.js
@@ -0,0 +1,17 @@
+// Using strict mode 
+    'use strict';
+
+// Ex.1 templat
+/* 
+  <div class="error">
+            <ul>
+                <li>
+                    <h4>Error</h4>
+                </li>
+                <li>
+                    <i class="fa-solid fa-xmark"></i>
+                </li>
+            </ul>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, vitae at. Dignissimos facere voluptas nobis sunt omnis deserunt saepe optio incidunt minima perferendis recusandae quos laborum, ducimus ipsum, illo eum.</p>
+    </div>
+*/
